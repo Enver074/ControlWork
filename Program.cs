1172c7b5adc4cb1e3865d5b71265f8f21d4ec41c
Enver Ismailov <113299648+Enver074@users.noreply.github.com>
2022-11-25 17:59:17 +0300

    Создание метода отбора

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
 ﻿Console.WriteLine("Введите элементы массива через запятую");
 string[] StringArray = Console.ReadLine()!.Split(',');
+
+string[] newStringArray = SelectString(StringArray);
+
+string[] SelectString(string[] StringArray)
+{
+    int size = StringArray.Length;
+    int newSize = 0;
+    int k = 0;
+    string[] newStringArray = new string[newSize];
+
+    for (int i = 0; i < size; i++)
+    {
+        if (StringArray[i].Length <= 3)
+        {
+            newSize++;
+            Array.Resize(ref newStringArray, newSize);
+            newStringArray[k] = StringArray[i];
+            k++;
+        }
+    }
+     return newStringArray;
+}
